8c1beadc25bfa35999bbdd48ae5ff54b9dbf02ce
Santiago Castañón <santiagocastanonarvizu@gmail.com>
2021-04-06 14:26:40 -0500

    basic layout

--- App.js
+++ App.js
@@ -1,13 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, FlatList, SafeAreaView } from 'react-native';
+import Header from './components/Header';
 
 export default function App() {
+  const [tasks, setTasks] = useState([
+      {"task":"HTML I","done":true,"id":"1"},
+      {"task":"CSS","done":true,"id":"2"},
+      {"task":"Responsive design","done":true,"id":"3"},
+  ]);  
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Header/>
+      <View style={styles.content}>
+        <View styles={styles.list}>
+            <FlatList
+                data={tasks}
+                renderItem={({item})=>(
+                    <Text>{item.task}</Text>
+                )}
+              /> 
+        </View>
+      </View>
+    </SafeAreaView>
   );
 }
 
@@ -15,7 +31,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+    content: {
+        padding:30
+    },
+    list:{
+        marginTop:30
+    }
 });
---
+++ README.md
@@ -0,0 +1,6 @@
+#   React Native ToDo App
+Using [Expo](https://docs.expo.io/)
+
+## Installation and Run
+1. `npm install --global expo-cli`
+2. `expo start`
